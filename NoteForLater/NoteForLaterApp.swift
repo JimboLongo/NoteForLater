f1269b55dcb8f1e40d23e0c89ae2ff06e6259939
Jimmy Long <jimmylong603@gmail.com>
2026-07-31 16:36:22 -0400

    Initial Commit

---
+++ NoteForLater/NoteForLaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteForLaterApp.swift
+//  NoteForLater
+//
+//  Created by Jimmy Long on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct NoteForLaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
